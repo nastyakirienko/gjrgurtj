7b68f473431802165d373dba5a99d146e61c0ea7
nastyakirienko <162957498+nastyakirienko@users.noreply.github.com>
2024-03-11 12:54:27 +0600

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,15 @@
+<?php
+
+// Input the three numbers
+// and store it in variable
+
+$number1 = 12;
+$number2 = 7;
+$number3 = 15;
+
+// Using the max function to find the largest number
+
+$maxNumber = max($number1, $number2, $number3);
+echo "The largest number among three is: $maxNumber\n";
+
+?>
